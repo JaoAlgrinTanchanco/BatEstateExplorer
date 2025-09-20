eb93857c25fbbd0c6c068c07230f5d7605ef8e85
John Ansel Doton <johnanseldoton@gmail.com>
2025-09-20 23:16:08 +0800

    desigining assoc(home[init])

--- public/app/Views/layout/agent_layout.php
+++ public/app/Views/layout/agent_layout.php
@@ -7,7 +7,7 @@ if (!isset($page_title)) $page_title = "Agent Dashboard";
   <meta charset="UTF-8">
   <title><?= htmlspecialchars($page_title) ?></title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="../../assets/css/admin_layout.css">
+  <link rel="stylesheet" href="../../assets/css/agent_layout.css">
   <link rel="stylesheet" href="../../assets/css/property_card.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
