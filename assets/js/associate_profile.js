3cf9043f8e7a5783de0d7c34430cfff571504da5
John Ansel Doton <johnanseldoton@gmail.com>
2025-09-21 21:36:11 +0800

    desigining assoc_home(card responsive[init])

--- assets/js/associate_profile.js
+++ assets/js/associate_profile.js
@@ -386,18 +386,3 @@ function markImageForRemoval(button, imagePath){
     container.appendChild(hiddenInput);
     button.disabled=true;
 }
-
-const burger = document.querySelector('.nav-burger');
-const dropdown = document.querySelector('.nav-dropdown');
-
-burger.addEventListener('click', () => {
-dropdown.style.display = dropdown.style.display === 'flex' ? 'none' : 'flex';
-});
-const footerSections = document.querySelectorAll('.footer-section');
-
-footerSections.forEach(section => {
-const header = section.querySelector('h3, h4');
-header.addEventListener('click', () => {
-    section.classList.toggle('active');
-});
-});
