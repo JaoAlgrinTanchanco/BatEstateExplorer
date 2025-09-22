8a57ef8164903d073de7eb126740e4c1ecc728b6
John Ansel Doton <johnanseldoton@gmail.com>
2025-09-22 10:08:25 +0800

    html fix

--- public/app/Views/agent/associate_profile.php
+++ public/app/Views/agent/associate_profile.php
@@ -221,6 +221,7 @@
 
 <link rel="stylesheet" href="/BatEstateExplorer/assets/css/associate_profile.css">
 <link rel="stylesheet" href="/BatEstateExplorer/assets/css/agent_sidebar.css">
+<link rel="stylesheet" href="/BatEstateExplorer/assets/css/agent_overview.css">
 <script src="https://www.paypal.com/sdk/js?client-id=AS2IFQyy2dcIowcsn3TnY5rSfvzbQbx3KrcGxSeaVBr9XoqYVqNrDR_hPHDXt3gUzhIr1vuUx1m4J1Yt&currency=PHP"></script>
 <div class="dashboard-container">
 
@@ -902,7 +903,7 @@
                 // Overview Tab
                 $fullName = trim(($user['first_name'] ?? '') . ' ' . ($user['last_name'] ?? ''));
                 if ($fullName === '') $fullName = 'Agent';?>
-                
+
                 <header class="content-header">
                     <h2>Profile Overview</h2>
                 </header>
