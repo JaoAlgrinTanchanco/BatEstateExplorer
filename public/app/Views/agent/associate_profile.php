6cc6f039cfd486fa8a777577eda2b04cb1487c32
John Ansel Doton <johnanseldoton@gmail.com>
2025-09-23 15:23:15 +0800

    desigining assoc(wallet[init])

--- public/app/Views/agent/associate_profile.php
+++ public/app/Views/agent/associate_profile.php
@@ -226,6 +226,7 @@
 <link rel="stylesheet" href="/BatEstateExplorer/assets/css/agent_add_listing.css">
 <link rel="stylesheet" href="/BatEstateExplorer/assets/css/agent_analytics.css">
 <link rel="stylesheet" href="/BatEstateExplorer/assets/css/company_listing.css">
+<link rel="stylesheet" href="/BatEstateExplorer/assets/css/agent_review.css">
 <script src="https://www.paypal.com/sdk/js?client-id=AS2IFQyy2dcIowcsn3TnY5rSfvzbQbx3KrcGxSeaVBr9XoqYVqNrDR_hPHDXt3gUzhIr1vuUx1m4J1Yt&currency=PHP"></script>
 <div class="dashboard-container">
 
